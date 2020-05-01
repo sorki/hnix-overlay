24ccfcc0b81dbaf18f985d762b7291df2d326904
Richard Marko <srk@48.io>
2020-05-01 12:16:35 +0200

    bump update-nix-file

--- nix/update-nix-file.nix
+++ nix/update-nix-file.nix
@@ -3,8 +3,8 @@ let
   src = super.fetchFromGitHub {
     owner = "sorki";
     repo = "update-nix-file";
-    rev = "233df3768217fa9ed0961726d1d3c088a28ee62a";
-    sha256 = "0kdqxq1rv6y9iz90xckwlajs1lka3hf19icdwdxkakn2x35hm6ai";
+    rev = "5a2db8d185e58ca9faa84a0d6aa8e9048f1d8cd6";
+    sha256 = "17hc6kzg4493qb9jin6y06ixzia37mhb9i2l9zi378k5ck16lh4k";
   };
 
   # swap with src to build from this path
