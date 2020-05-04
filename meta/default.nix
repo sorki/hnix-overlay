810ab83748155d5d8773172833d1e52536b97633
Richard Marko <srk@48.io>
2020-05-04 12:50:56 +0200

    update meta

--- meta/default.nix
+++ meta/default.nix
@@ -3,8 +3,8 @@ let
   hnSrc = nixpkgs.fetchFromGitHub {
     owner = "sorki";
     repo = "hnix-overlay";
-    rev = "860fb41b4d489e536634fde23adb808c8cf337f1";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    rev = "a958fb56140e75cf0fed5b8100508236b511e890";
+    sha256 = "0iqj1l94lh2bhl5y6bs30zh5207m53784lly2zw763gs304rdb52";
   };
   hn = import hnSrc {};
   src = hn.pkgs.nix-gitignore.gitignoreSource [] ./.;
