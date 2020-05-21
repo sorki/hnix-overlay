bbaae3f0e1a2511be4f8d31ec39bd163f1f2ca95
Richard Marko <srk@48.io>
2020-05-21 13:39:40 +0200

    bump nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -2,8 +2,8 @@
 # $ nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/${REVISION}.tar.gz
 #
 let
-  rev="77a062fe47940f1d1cdfb136934bb42d20094c51";
-  hash="0a19fnk18pl31a66g7k81hgjxqazbbx78n5qmmn0s993m67sx5z4";
+  rev="0010ae4960d35243e7abb046cd26ddda904a4c63";
+  hash="1y55mfm6cz7dwa8jk9873w4wpqp693mw4xfazq5k52wg48d9sfxd";
 in
 import (
   fetchTarball {
